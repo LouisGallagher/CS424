6203e8c1bba15e31cd985e0684c1b5ee7ab0da0e
GALLAGHER LOUIS PATRICK <se415023@CS4-50>
2015-11-09 17:45:21 +0000

    set operations

--- scheme/assignment.scm
+++ scheme/assignment.scm
@@ -16,7 +16,22 @@
 (define set-union
 	(lambda (s1 s2)
 		(cond
-			((null? s2) (cons s1 null))
-			((set-contains? s1 (car s2)) (set-union s1 (cdr s2)))
-			(else (set-union (list s1 (car s2)) (cdr s2))))))
+			((null? s2) s1 )
+			((null? s1) s2 )
+			((set-contains? s1 (car s2)) (cons (car s1) (set-union (cdr s1) (cdr s2))))
+			(else (cons (car s1) (cons (car s2) (set-union (cdr s1) (cdr s2)))))))) 
 
+(define set-intersection
+	(lambda (s1 s2)
+		(cond
+			((null? s1) null)
+			((set-contains? s2 (car s1)) (cons (car s1) (set-intersection (cdr s1) s2)))
+			(else (set-intersection (cdr s1) s2)))))
+
+(define set-difference
+	(lambda (s1 s2)
+		(cond
+			((null? s1) s2)
+			((null? s2) s1)
+			((set-contains? s1 (car s2)) (set-difference s1 (cdr s2)))
+			(else (cons (car s2) (set-difference s1 (cdr s2)))))))
