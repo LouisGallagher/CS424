08f78b212f89a32f7468bbb00ee355156fc67a11
GALLAGHER LOUIS PATRICK <se415023@CS4-50>
2015-11-10 10:36:49 +0000

    set-difference

--- scheme/assignment.scm
+++ scheme/assignment.scm
@@ -31,7 +31,6 @@
 (define set-difference
 	(lambda (s1 s2)
 		(cond
-			((null? s1) s2)
-			((null? s2) s1)
-			((set-contains? s1 (car s2)) (set-difference s1 (cdr s2)))
-			(else (cons (car s2) (set-difference s1 (cdr s2)))))))
+			((null? s1) null)  
+			((set-contains? s2 (car s1)) (set-difference (cdr s1) s2))
+			(else (cons (car s1) (set-difference (cdr s1) s2))))))
