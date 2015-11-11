049b1561955ab7c4936c2d415a82b2070ce1d135
GALLAGHER LOUIS PATRICK <se415023@CS4-50>
2015-11-11 12:43:18 +0000

    finished beta-reduction code

--- scheme/assignment.scm
+++ scheme/assignment.scm
@@ -28,8 +28,9 @@
 (define set-union
 	(lambda (s1 s2)
 		(cond
-			((null? s2) (list s1))
-			((null? s1) (list s2))
+			((and (null? s1) (null? s2)) null)
+			((null? s2) (cons (car s1) (set-union (cdr s1) s2)))
+			((null? s1) (cons (car s2) (set-union  s1 (cdr s2))))
 			((set-contains? s1 (car s2)) (set-union  s1 (cdr s2)))
 			(else (cons (car s2) (set-union  s1 (cdr s2)))))))
 
@@ -72,7 +73,7 @@
 		(cond
 			((null? e) null)
 			((list? (car e)) (set-union (bound-variables(car e)) (bound-variables (cdr e))))
-			((equal? (car e) 'λ) (set-union (cadr e) (bound-variables (cddr e))))
+			((equal? (car e) 'λ) (set-union (list (cadr e)) (bound-variables (cddr e))))
 			(else (bound-variables (cdr e))))))
 
 ;; Gets the set of free variables in a lambda calculus expression 
@@ -104,7 +105,7 @@
 				(pair? e)
 				 (and 
 				 	(equal? (caar e) 'λ) 
-				 	(null? (set-intersection (bound-variables (cddar e)) (free-variables (cdr e)) ))))) 
-			(replace (cddar e) (cdr e) (cadar e))                                                                                                                                   
+				 	(null? (set-intersection (bound-variables (cddar e)) (free-variables (cdr e)) )))) 
+			(replace (cddar e) (cdr e) (cadar e)))                                                                                                                                   
 			(else #f))))
 
