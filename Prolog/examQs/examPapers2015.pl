fdfeac9c5c9b6833aa101a02627ef74b2cc0f46c
Louis <louis.gallagher.2013@mumail.ie>
2015-12-29 21:09:37 +0000

    stuff

--- Prolog/examQs/examPapers2015.pl
+++ Prolog/examQs/examPapers2015.pl
@@ -2,3 +2,8 @@
 
 noah([], [], []).
 noah([X | Xs], [Y | Ys], [X,Y | Zs]) :- noah(Xs, Ys, Zs).
+
+member(X,[X | _ ]).
+member(X, [Y | Ys]) :- member(X, Ys).
+
+doubleMember(X, [ Y | Ys]) :- X==Y, member(X, Ys); doubleMember(X, Ys). 
--- haskell/examQs/examPapers2015.hs
+++ haskell/examQs/examPapers2015.hs
@@ -1,8 +1,15 @@
 import Data.List
 
---revCount :: [a] -> [Int] -> [a]
+revCount :: [a] -> [Int] -> [a]
 
 revCount [x] [0] = [] 
 revCount [] [] = []
 revCount (x:[]) (c:[]) = [x] ++ revCount [x] [c-1]
 revCount (x:xs) (c:cs) = revCount xs cs ++ revCount [x] [c]
+
+
+afterFilter :: (a -> Bool) -> [a] -> [a]
+
+afterFilter p [] = [] 
+afterFilter p [_] = []
+afterFilter p (x:xs) = if p x then [head xs] ++ afterFilter p xs else afterFilter p xs
