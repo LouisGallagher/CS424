4fe54e04af755f15234a0f9fab7687298409332f
Louis <louis.gallagher.2013@mumail.ie>
2016-01-01 22:07:35 +0000

    2014 Aug q3

--- Prolog/examQs/examPapers2015.pl
+++ Prolog/examQs/examPapers2015.pl
@@ -4,6 +4,13 @@ noah([], [], []).
 noah([X | Xs], [Y | Ys], [X,Y | Zs]) :- noah(Xs, Ys, Zs).
 
 member(X,[X | _ ]).
-member(X, [Y | Ys]) :- member(X, Ys).
+member(X, [_| Ys]) :- member(X, Ys).
 
 doubleMember(X, [ Y | Ys]) :- X==Y, member(X, Ys); doubleMember(X, Ys). 
+
+
+sibling([[Z,X],[Z,Y]],X,Y) :- X \== Y.
+sibling([[Z,Y],[Z,X]],X,Y) :- X \== Y.
+sibling([Z, X], X, Y).
+sibling([Z, Y], X, Y).
+sibling([], X, Y):-.
